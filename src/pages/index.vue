08203966fca33f9abf9bd7f664c6391b5415b809
asrorbek-mannonov <asrorbekmannonov2016@gmail.com>
2022-12-11 09:12:45 +0500

    fix(auth): redirect to store on login

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,56 +1,29 @@
-<script setup lang="ts">
-defineOptions({
-  name: 'IndexPage',
-})
-const user = useUserStore()
-const name = $ref(user.savedName)
-
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
-
-const { t } = useI18n()
+<script lang="ts" setup>
+import { useTodo } from '~/store/todo'
+const store = useTodo()
 </script>
 
 <template>
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
-    </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        Vitesse
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
-
-    <div py-4 />
-
-    <TheInput
-      v-model="name"
-      placeholder="What's your name?"
-      autocomplete="false"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        btn m-3 text-sm
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
-    </div>
-  </div>
+  <h1 class="title">
+    Todo App
+  </h1>
+  <p class="subtitle">
+    {{ store.getCompletedCount }}/{{ store.getTotal }} completed
+  </p>
+  <TodoList />
 </template>
 
-<route lang="yaml">
-meta:
-  layout: home
-</route>
+<style scoped lang='scss'>
+.title {
+  color: $red;
+  font-size: 48px;
+  line-height: 56px;
+  margin-bottom: 22px;
+}
+.subtitle {
+ color: $black;
+ font-size: 24px;
+ line-height: 28px;
+ margin-bottom: 22px;
+}
+</style>
